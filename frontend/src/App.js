95540895a51c38035f0ae610f5295cb87edfa82e
herman <hermanpineda94@gmail.com>
2022-02-18 18:33:00 -0600

    protect project page with board if user is not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,23 @@ import {
   Routes
 } from 'react-router-dom';
 
+import { ProvideAuth } from './useAuth';
 import Landing from '../src/components/Landing';
 import Board from '../src/components/Board';
+import PrivateRoute from '../src/components/PrivateRoute';
 
 function App() {
   return (
-    <div>
+    <ProvideAuth>
       <Router>
         <Routes>
           <Route exact path='/' element={<Landing />} />
-          <Route exact path='/project' element={<Board />} />
+          <Route exact path='/project' element={<PrivateRoute />}>
+            <Route exact path='/project' element={<Board />} />
+          </Route>/>
         </Routes>
       </Router>
-    </div>
+    </ProvideAuth>
   );
 }
 
