8f0fd12f12aae055bf98deefa81f2b39d7277737
herman <hermanpineda94@gmail.com>
2022-03-04 15:54:47 -0600

    add icons for issue priorities

---
+++ frontend/src/components/icons/IssuePriorityMedium.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssuePriorityMedium = () => {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='#e97f33' className='bi bi-arrow-up' viewBox='0 0 18 18'>
+            <path fillRule='evenodd' d='M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z' />
+        </svg>
+    );
+};
+
+export default IssuePriorityMedium;
