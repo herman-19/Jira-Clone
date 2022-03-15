750a67ce8110fe1693128fc24bc56808f8248b63
herman <hermanpineda94@gmail.com>
2022-03-15 16:28:39 -0500

    remove empty lines

---
+++ frontend/src/components/IssueStatusDropdown.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Dropdown } from 'semantic-ui-react';
+
+// Note the semantic-ui-react Dropdown component warns of findDomNode deprecation.
+// Discussion: https://github.com/Semantic-Org/Semantic-UI-React/issues/4050
+// See for progress: https://github.com/Semantic-Org/Semantic-UI-React/pull/4233
+// To be fixed in semantic ui react v3.
+
+const IssueStatusDropdown = () => {
+    const [selected, setSelected] = useState('BACKLOG');
+    const issueStatuses = [
+        {
+            key: 'BACKLOG',
+            text: 'BACKLOG',
+            value: 'BACKLOG'
+        },
+        {
+            key: 'SELECTED FOR DEVELOPMENT',
+            text: 'SELECTED FOR DEVELOPMENT',
+            value: 'SELECTED FOR DEVELOPMENT'
+        },
+        {
+            key: 'IN PROGRESS',
+            text: 'IN PROGRESS',
+            value: 'IN PROGRESS'
+        },
+        {
+            key: 'DONE',
+            text: 'DONE',
+            value: 'DONE'
+        },
+    ];
+
+    // Handle selected dropdown issue type.
+    const onChange = async (e, { value }) => {
+        // Update issue type here...
+        e.persist();
+        setSelected(value);
+        console.log(value);
+    };
+
+    return (
+        <Dropdown
+            id='dropdown-issue-status'
+            selection
+            defaultValue={selected} // Note, this corresponds to the 'value' attribute in the types.
+            options={issueStatuses}
+            button={true}
+            onChange={onChange}
+            style={{ maxHeight: "2px" }}
+        />
+    );
+};
+
+export default IssueStatusDropdown;
---
+++ frontend/src/components/IssueTypeDropdown.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Dropdown } from 'semantic-ui-react';
+import BugIcon from './icons/IssueTypeBug';
+import StoryIcon from './icons/IssueTypeStory';
+import TaskIcon from './icons/IssueTypeTask';
+
+// Note the semantic-ui-react Dropdown component warns of findDomNode deprecation.
+// Discussion: https://github.com/Semantic-Org/Semantic-UI-React/issues/4050
+// See for progress: https://github.com/Semantic-Org/Semantic-UI-React/pull/4233
+// To be fixed in semantic ui react v3.
+
+const dropdownIconStyle = {
+    marginRight: '0.78571429rem',
+    height: 28,
+    width: 28,
+    display: 'inline-block',
+    verticalAlign: 'top',
+    marginTop: '-0.5em',
+    marginBottom: '-0.5em',
+    maxHeight: '2em'
+};
+
+const IssueTypeDropdown = () => {
+    const [selected, setSelected] = useState('STORY');
+    const issueTypes = [
+        {
+            key: 'BUG',
+            text: 'BUG',
+            value: 'BUG',
+            image: (<BugIcon style={dropdownIconStyle} />)
+        },
+        {
+            key: 'STORY',
+            text: 'STORY',
+            value: 'STORY',
+            image: (<StoryIcon style={dropdownIconStyle} />),
+        },
+        {
+            key: 'TASK',
+            text: 'TASK',
+            value: 'TASK',
+            image: (<TaskIcon style={dropdownIconStyle} />)
+        },
+    ];
+
+    // Handle selected dropdown issue type.
+    const onChange = async (e, { value }) => {
+        // Update issue type here...
+        e.persist();
+        setSelected(value);
+        console.log(value);
+    };
+
+    return (
+        <Dropdown
+            id='dropdown-issue-type'
+            selection
+            defaultValue={selected} // Note, this corresponds to the 'value' attribute in the types.
+            options={issueTypes}
+            button={true}
+            onChange={onChange}
+            style={{ maxHeight: "2px" }}
+        />
+    );
+};
+
+export default IssueTypeDropdown;
