c5100c41c8b517f0b26c2c427fbf3880145c75d6
herman <hermanpineda94@gmail.com>
2022-03-14 23:36:16 -0500

    open modal when clicking on an issue card

--- frontend/src/components/IssueCard.js
+++ frontend/src/components/IssueCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskIcon from './icons/IssueTypeTask';
 import BugIcon from './icons/IssueTypeBug';
 import StoryIcon from './icons/IssueTypeStory';
@@ -7,6 +7,12 @@ import LowPrioIcon from './icons/IssuePriorityLow';
 import MedPrioIcon from './icons/IssuePriorityMedium';
 import HighPrioIcon from './icons/IssuePriorityHigh';
 import HighestPrioIcon from './icons/IssuePriorityHighest';
+import Modal from './Modal';
+
+const cardIconStyle = {
+    height: 18,
+    width: 18
+};
 
 const getIssuePrioIcon = (prio) => {
     let icon;
@@ -24,32 +30,45 @@ const getIssuePrioIcon = (prio) => {
 const getIssueTypeIcon = (type) => {
     let icon;
     switch (type) {
-        case 'BUG': icon = <BugIcon />; break;
-        case 'TASK': icon = <TaskIcon />; break;
-        case 'STORY': icon = <StoryIcon />; break;
+        case 'BUG': icon = <BugIcon style={cardIconStyle} />; break;
+        case 'TASK': icon = <TaskIcon style={cardIconStyle} />; break;
+        case 'STORY': icon = <StoryIcon style={cardIconStyle} />; break;
         default: icon = <TaskIcon />; break;
     }
     return icon;
 };
 
 const IssueCard = ({ title, type, prio }) => {
-    return (
-        <div className='issue-card'>
-            {title}
-            <div className='issue-card-info'>
-                <div className='issue-card-status'>
-                    {getIssueTypeIcon(type)}
-                    {getIssuePrioIcon(prio)}
-                </div>
+    const [displayModal, setDisplayModal] = useState(false);
+    const toggleModal = () => {
+        setDisplayModal(!displayModal);
+    };
 
-                <div className='issue-card-user-icons-container'>
-                    <div className='issue-card-user-icon-container'>
-                        <div className='card-user-icon' />
-                        <div className='card-user-icon' />
+    const data = {
+        title: title,
+        type: type,
+        prio: prio
+    };
+
+    return (
+        <div>
+            <div className='issue-card' onClick={toggleModal}>
+                {title}
+                <div className='issue-card-info'>
+                    <div className='issue-card-status'>
+                        {getIssueTypeIcon(type)}
+                        {getIssuePrioIcon(prio)}
+                    </div>
+
+                    <div className='issue-card-user-icons-container'>
+                        <div className='issue-card-user-icon-container'>
+                            <div className='card-user-icon' />
+                            <div className='card-user-icon' />
+                        </div>
                     </div>
                 </div>
-
             </div>
+            <Modal isDiplayed={displayModal} toggleModal={toggleModal} data={data} />
         </div>
     );
 };
