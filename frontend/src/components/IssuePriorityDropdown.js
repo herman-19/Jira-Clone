1778697255b3414d48ef2b7272592b44a9f5dda9
herman <hermanpineda94@gmail.com>
2022-03-21 02:04:09 -0500

    remove unused imported components and styling for priority dropdown

--- frontend/src/components/IssuePriorityDropdown.js
+++ frontend/src/components/IssuePriorityDropdown.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
 import { Dropdown } from 'semantic-ui-react';
-import Highest from './icons/IssuePriorityHighest';
-import High from './icons/IssuePriorityHigh';
-import Medium from './icons/IssuePriorityMedium';
-import Low from './icons/IssuePriorityLow';
-import Lowest from './icons/IssuePriorityLowest';
 
 // Note the semantic-ui-react Dropdown component warns of findDomNode deprecation.
 // Discussion: https://github.com/Semantic-Org/Semantic-UI-React/issues/4050
 // See for progress: https://github.com/Semantic-Org/Semantic-UI-React/pull/4233
 // To be fixed in semantic ui react v3.
-
-// const dropdownPrioStyle = {
-//     height: 18,
-//     width: 18,
-//     marginRight: '8px'
-// };
 
 const IssuePriorityDropdown = () => {
     const [selected, setSelected] = useState('Highest');
@@ -23,34 +12,27 @@ const IssuePriorityDropdown = () => {
         {
             key: 'Highest',
             text: 'Highest',
-            value: 'Highest',
-            className: 'font-size-12px',
-            // image: (<Highest style={dropdownPrioStyle} />)
+            value: 'Highest'
         },
         {
             key: 'High',
             text: 'High',
-            value: 'High',
-            className: 'font-size-12px',
-            // image: (<High style={dropdownPrioStyle} />),
+            value: 'High'
         },
         {
             key: 'Medium',
             text: 'Medium',
-            value: 'Medium',
-            // image: (<Medium style={dropdownPrioStyle} />)
+            value: 'Medium'
         },
         {
             key: 'Low',
             text: 'Low',
-            value: 'Low',
-            // image: (<Low style={dropdownPrioStyle} />)
+            value: 'Low'
         },
         {
             key: 'Lowest',
             text: 'Lowest',
-            value: 'Lowest',
-            // image: (<Lowest style={dropdownPrioStyle} />)
+            value: 'Lowest'
         },
     ];
 
