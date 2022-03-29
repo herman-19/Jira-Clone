1f3ad5fe9e3fb69eb7b378ab92070c60f5d89849
Herman Pineda <hermanpineda@Hermans-MacBook-Pro-2.local>
2022-03-28 23:11:49 -0500

    implement Comments component to render list of Comment components

---
+++ frontend/src/components/Comments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Comment from './Comment';
+
+const Comments = ({comments, users}) => {
+    const idToAuthor = new Map();
+    for (let c of comments) {
+        idToAuthor.set(c.person_id, users[users.findIndex(u => u.person_id === c.person_id)].name);
+    }
+    const c = comments.map(com => <Comment key={com.comment_id} author={idToAuthor.get(com.person_id)} timestamp={com.created_at} text={com.content}/> );
+    return (
+        <div>
+            {c}
+        </div>
+    );
+};
+
+export default Comments;
