b9058a0bfb41723bb7574f2c1b9e6f139d004860
herman <hermanpineda94@gmail.com>
2022-04-03 22:55:03 -0500

    adjust timestamp styling for comment component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 const Comment = ({ author, timestamp, text }) => {
     return (
@@ -6,7 +7,7 @@ const Comment = ({ author, timestamp, text }) => {
             <div id='new-comment-user-img' />
             <div id='pad-left'>
                 <div id='user-comment-author'>{author}</div>
-                <div id='user-comment-timestamp'>{timestamp}</div>
+                <div id='user-comment-timestamp'>{format(new Date(timestamp), "MMM d, yyyy, h:mm:ss a")}</div>
                 <p id='user-comment'>{text}</p>
             </div>
         </div>
