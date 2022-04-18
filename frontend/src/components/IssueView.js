7218fde22d5c036897c34058c171e41ac44145c4
herman <hermanpineda94@gmail.com>
2022-04-18 01:01:12 -0500

    fetch project info when for single issue view

--- frontend/src/components/IssueView.js
+++ frontend/src/components/IssueView.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Issue from './Issue';
 import { useParams } from 'react-router-dom';
+import { fetchProjectInfo } from '../api/UserAPI';
 
 const IssueView = () => {
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+
+    useEffect(() => {
+        const getProjectInfo = async () => {
+            const info = await fetchProjectInfo();
+            setName(info.name);
+            setCategory(info.category);
+        };
+        getProjectInfo();
+    }, []);
+
     let { id } = useParams();
     return (
         <div className='container'>
             <Navbar />
-            <Sidebar />
+            <Sidebar name={name} category={category}/>
             <Issue issueId={id} />
         </div>
     );
