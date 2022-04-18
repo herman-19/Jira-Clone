348ec782a1fb44f37d20a50799ee9e1a4739adee
herman <hermanpineda94@gmail.com>
2022-04-18 00:58:07 -0500

    introduce search pane component to the navigation bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AtlassianLogo from './AtlassianLogoWhite';
 import SearchIcon from './icons/Search';
 import AddIcon from './icons/Add';
+import SearchIssuePane from './SearchIssuePane';
 
 const Navbar = () => {
+    const [searchPaneDisplayed, setSearchPaneDisplayed] = useState(false);
+    const onSearchClick = () => {
+        // display pane for search
+        setSearchPaneDisplayed(true);
+    };
+
     return (
         <div id='navbar-left'>
             <AtlassianLogo />
-            <p id='navbar-left-item'><SearchIcon w='20' h='20' /></p>
-            <p id='navbar-left-item'><AddIcon /></p>
+            <p id='navbar-left-item' onClick={onSearchClick} className='tooltip'><SearchIcon w='20' h='20' /><span className='tooltiptext'>Search issue</span></p>
+            <p id='navbar-left-item' className='tooltip'><AddIcon /><span className='tooltiptext'>Create issue</span></p>
+            <SearchIssuePane isDisplayed={searchPaneDisplayed} setIsDisplayed={setSearchPaneDisplayed}/>
         </div>
     );
 };
