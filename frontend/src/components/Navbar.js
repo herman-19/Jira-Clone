2ab26426fc2b42d7ad14b33e4a70aed8e18ca42e
herman <hermanpineda94@gmail.com>
2022-04-22 01:26:37 -0500

    display modal for creating new issue upon clicking the add icon in the navigation bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CreateIssueModal from './CreateIssueModal';
 import AtlassianLogo from './AtlassianLogoWhite';
 import SearchIcon from './icons/Search';
 import AddIcon from './icons/Add';
@@ -10,13 +11,18 @@ const Navbar = () => {
         // display pane for search
         setSearchPaneDisplayed(true);
     };
+    const [displayModal, setDisplayModal] = useState(false);
+    const toggleModal = () => {
+        setDisplayModal(!displayModal);
+    };
 
     return (
         <div id='navbar-left'>
             <AtlassianLogo />
             <p id='navbar-left-item' onClick={onSearchClick} className='tooltip'><SearchIcon w='20' h='20' /><span className='tooltiptext'>Search issue</span></p>
-            <p id='navbar-left-item' className='tooltip'><AddIcon /><span className='tooltiptext'>Create issue</span></p>
+            <p id='navbar-left-item' onClick={toggleModal} className='tooltip'><AddIcon /><span className='tooltiptext'>Create issue</span></p>
             <SearchIssuePane isDisplayed={searchPaneDisplayed} setIsDisplayed={setSearchPaneDisplayed}/>
+            <CreateIssueModal isDiplayed={displayModal} toggleModal={toggleModal} />
         </div>
     );
 };
