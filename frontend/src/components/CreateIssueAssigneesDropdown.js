1d18faf97eb645715629af98f500be8d3c55df29
herman <hermanpineda94@gmail.com>
2022-04-22 01:20:39 -0500

    create assignees dropdown component used in modal for creating issue

---
+++ frontend/src/components/CreateIssueAssigneesDropdown.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { Dropdown } from 'semantic-ui-react';
+
+const CreateIssueAssigneesDropdown = ({ users, onAssigneesSelect}) => {
+    const [selected, setSelected] = useState([]);
+    const [displayDropdown, setDisplayDropdown] = useState(true);
+
+    const [assigneeOptions, setAssigneeOptions] = useState(null);
+    useEffect(() => {
+        if (users) {
+            const options = [];
+            for (let user of users) {
+                options.push({
+                    name: user.person_id,
+                    text: user.name,
+                    value: user.name
+                });
+            }
+            setAssigneeOptions(options);
+        }
+    }, [users]);
+
+    const onChange = async (e, { value }) => {
+        // Update assignees here...
+        e.persist();
+        setSelected(value);
+        console.log(value);
+
+        // Find assignee ids of selected assignees.
+        let newAssigneeIDs = [];
+        for (let assigneeName of value) {
+            let idx = users.findIndex(u => u.name === assigneeName);
+            if (idx !== -1) {
+                newAssigneeIDs.push(users[idx].person_id);
+            }
+        }
+        onAssigneesSelect(newAssigneeIDs);
+    };
+
+    return (
+        <div>
+            {
+                (selected.length > 0 || displayDropdown)
+                &&
+                <Dropdown
+                    multiple selection
+                    value={selected}
+                    options={assigneeOptions}
+                    button={true}
+                    onChange={onChange}
+                    search={true}
+                    placeholder='Search assignee(s)'
+                    style={{width: '100%', fontWeight: 'light'}}
+                />
+            }
+        </div >
+    );
+};
+
+export default CreateIssueAssigneesDropdown;
