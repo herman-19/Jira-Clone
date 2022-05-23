5bd8dd4c562ec56792c1cab3d28fe0ecc7ee325d
herman <hermanpineda94@gmail.com>
2022-05-22 19:14:01 -0500

    create pm2 config file used for deployment of server

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,21 @@
+const conf = require('config');
+const sshPath = conf.get('sshPath');
+const ec2DNS = conf.get('ec2DNS');
+
+module.exports = {
+    apps: [{
+      name: 'Jira-Clone-Server',
+      script: './server.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: ec2DNS,
+        key: sshPath,
+        ref: 'origin/main',
+        repo: 'https://github.com/herman-19/Jira-Clone.git',
+        path: '/home/ubuntu/server',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  };
