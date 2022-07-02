7516835e47e8289565e8b0a3b5f87ba57a0b50fb
herman <hermanpineda94@gmail.com>
2022-07-02 00:45:53 -0500

    remove ability to send cross-site Access-Control requests using credentials

--- frontend/src/api/http-common.js
+++ frontend/src/api/http-common.js
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:8080", // TODO: update to server base url based on production environment
-  withCredentials: true
 });
